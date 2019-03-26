ad02acd23f5f8f86fd83e3be6467ebc9e351bcf4
Adrian Jeż <adrian.jez@ingbank.pl>
2019-03-26 12:45:41 +0100

    + Fixed release building - attached configuration with keystore + Fixed lint issues (unsafe class loader calling)

--- SongApp/data/src/main/java/com/hqapps/data/utils/ReadJsonFileUtils.kt
+++ SongApp/data/src/main/java/com/hqapps/data/utils/ReadJsonFileUtils.kt
@@ -1,16 +1,25 @@
 package com.hqapps.data.utils
 
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
-import java.util.stream.Collectors
 
 object ReadJsonFileUtils {
-    fun loadJsonFileFromResources(path: String): String{
-        val inn = BufferedReader(InputStreamReader(
-                javaClass.classLoader.getResourceAsStream(path), "UTF-8"
-        ))
-        return inn.lines()
-                .parallel()
-                .collect(Collectors.joining())
+
+    fun loadJsonFileFromResources(path: String): String {
+        try {
+            val sb = StringBuilder()
+            val reader = BufferedReader(InputStreamReader(
+                    javaClass.classLoader?.getResourceAsStream(path), "UTF-8"))
+            var line: String?
+            while (true) {
+                line = reader.readLine()
+                if (line != null) sb.append(line)
+                else break
+            }
+            return sb.toString()
+        } catch (e: IOException) {
+            throw IllegalArgumentException("Could not read from resource at: $path")
+        }
     }
 }
